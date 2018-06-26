b0d38fc03e4ef12674cc918f2e5dcdb761cdfba6
dsclementsen <davur@davur.net>
2018-06-26 21:52:23 +1000

    Checkpoint - serverless working

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
 
+import yaml
+# import simplejson as json
+
 import boto3
 from botocore.exceptions import ClientError
 
@@ -9,6 +12,7 @@ from botocore.exceptions import ClientError
 app = Flask(__name__)
 CORS(app)
 
+BUCKET_NAME = "env.innablr.xyz"
 
 @app.route("/")
 def hello():
@@ -38,7 +42,7 @@ def get_defined_stacks():
     client = get_client('s3')
     files = []
     # client.list_objects(Bucket='bucket_name', Prefix='prefix_string')['Contents']
-    objects = client.list_objects(Bucket='env.innablr.xyz', Prefix='stacks/')
+    objects = client.list_objects(Bucket=BUCKET_NAME, Prefix='stacks/')
 #     for item in client.list_objects(Bucket='env.innablr.xyz', Prefix='stacks/')['Contents']:
 #         files.append(item['Key'])
     return jsonify(objects)
@@ -46,12 +50,31 @@ def get_defined_stacks():
 @app.route("/api/templates")
 def get_templates():
     client = get_client('s3')
-    files = []
+    files = {}
     # client.list_objects(Bucket='bucket_name', Prefix='prefix_string')['Contents']
-    objects = client.list_objects(Bucket='env.innablr.xyz', Prefix='templates/')
-#     for item in client.list_objects(Bucket='env.innablr.xyz', Prefix='stacks/')['Contents']:
-#         files.append(item['Key'])
-    return jsonify(objects)
+    objects = client.list_objects(Bucket=BUCKET_NAME, Prefix='templates/')
+
+    for item in objects['Contents']:
+        # obj = client.get_object(Bucket=bucket, Key=key)
+        # j = json.loads(obj['Body'].read())
+        print(item)
+        common_prefix = 'templates/'
+        name = item['Key'][len(common_prefix):]
+        if len(name) > 0:
+            files[item['Key']] = {
+                'key': item['Key'],
+                'name': name,
+                'modified': item['LastModified'],
+            }
+
+    templates = []
+    for key, item in files.items():
+        object = client.get_object(Bucket=BUCKET_NAME, Key=key)
+        item['content'] = object['Body'].read().decode('utf-8')
+        templates.append(item)
+
+    return jsonify({'templates': templates})
+
 
 @app.route('/api/stacks', defaults={'stack': None})
 @app.route('/api/stacks/<stack>')
@@ -74,6 +97,89 @@ def get_current_stacks(stack=None):
         }
 
     return jsonify(stacks)
+
+@app.route('/api/templates/<template>/quicklaunch/<stack>')
+def launch_from_template(template, stack):
+
+    cloudformation = """
+AWSTemplateFormatVersion: '2010-09-09'
+Metadata:
+  License: Apache-2.0
+Description: 'Single EC2 install and run httpd service'
+Resources:
+  EnvStack:
+    Type: AWS::CloudFormation::Stack
+    Properties:
+      TemplateURL: https://s3-ap-southeast-2.amazonaws.com/env.innablr.xyz/templates/%s
+      TimeoutInMinutes: 4
+      Parameters:
+        KeyName: davur-innablr-dev
+  DnsStack:
+    Type: AWS::CloudFormation::Stack
+    Properties:
+      TemplateURL: https://s3-ap-southeast-2.amazonaws.com/env.innablr.xyz/templates/env-dns-records.yaml
+      Parameters:
+        EnvName: %s
+        EnvDns: !GetAtt [EnvStack, Outputs.PublicDNS]
+        RecordType: CNAME
+Outputs:
+  Dns:
+    Description: DNS entry for new environment
+    Value: %s.env.innablr.env
+""" % (template, stack, stack)
+
+    client = get_client('cloudformation')
+    stack_name = "wildcardenv-%s" % stack
+    response = client.create_stack(
+           StackName=stack_name,
+           TemplateBody=cloudformation,
+           #Parameters=parameters_as_array(env.parameters),
+        )
+    # print(response)
+
+    if 'StackId' in response:
+        returnValue = {
+            "id": "wildcardenv-%s" % stack,
+            "stack_id": response['StackId'],
+            "name": stack,
+            "parameters": "",
+            "stack_id": response['StackId'],
+            "status": "CREATE_IN_PROGRESS",
+        }
+    else:
+        {"Error": "Create Failed"}
+
+    return jsonify(returnValue)
+
+
+# @detail_route(methods=['get', 'post',])
+# def launch(self, request, pk=None):
+#     env = self.get_object()
+#
+#     if env.stack_id:
+#         return self.refresh(request, pk)
+#
+#
+#     client = get_client('cloudformation')
+#     stack_name = "wildcardenv-%s" % env.name
+#     response = client.create_stack(
+#            StackName=stack_name,
+#            TemplateBody=env.template.body,
+#            Parameters=parameters_as_array(env.parameters),
+#         )
+#     print(response)
+#
+#     if 'StackId' in response:
+#         env.stack_id = response['StackId']
+#         env.status = 'CREATE_IN_PROGRESS'
+#         env.save()
+#
+#     # return Response({'StackId': 'success'})
+#     serializer = serializers.EnvironmentSerializer(env)
+#     return Response(serializer.data)
+
+
+
 
 
 #     def refresh(self, request, pk=None):
@@ -123,3 +229,54 @@ def parameters_as_array(parameters):
                 'ParameterValue': value,
             })
     return parameter_array
+
+
+# from __future__ import print_function
+from google.auth.transport import requests
+from google.oauth2 import id_token
+
+# https://blog.codecentric.de/en/2018/04/aws-lambda-authorizer/
+def generatePolicy(principalId, effect, methodArn):
+    authResponse = {}
+    authResponse['principalId'] = principalId
+
+    if effect and methodArn:
+        policyDocument = {
+            'Version': '2012-10-17',
+            'Statement': [
+                {
+                    'Sid': 'FirstStatement',
+                    'Action': 'execute-api:Invoke',
+                    'Effect': effect,
+                    'Resource': methodArn
+                }
+            ]
+        }
+
+        authResponse['policyDocument'] = policyDocument
+
+    return authResponse
+
+def lambda_handler(event, context):
+    try:
+        # Verify and get information from id_token
+        idInformation = id_token.verify_oauth2_token(
+            event['authorizationToken'],
+            requests.Request(),
+            '919361216275-tiobj6ratp5cvt8fie6vga30tkh50tf0.apps.googleusercontent.com')
+        print(idInformation)
+
+        # Deny access if the account is not a Google account
+        if idInformation['iss'] not in ['accounts.google.com',
+            'https://accounts.google.com']:
+            return generatePolicy(None, 'Deny', event['methodArn'])
+
+        # Get principalId from idInformation
+        principalId = idInformation['sub']
+
+    except ValueError as err:
+        # Deny access if the token is invalid
+        print(err)
+        return generatePolicy(None, 'Deny', event['methodArn'])
+
+    return generatePolicy(principalId, 'Allow', event['methodArn'])
---
+++ ui/app.js
@@ -0,0 +1,224 @@
+var env_status_map =  {
+	'': 'Not running',
+	'CREATE_IN_PROGRESS': 'Starting...',
+	'CREATE_COMPLETE': 'Running',
+	'UPDATE_COMPLETE': 'Running',
+	'DELETE_IN_PROGRESS': 'Stopping...',
+	'DELETED': 'Stopped',
+	'ROLLBACK_COMPLETE': 'Failed to start',
+	'DELETE_FAILED': 'Failed'
+}
+var all_status_classes = "CREATE_IN_PROGRESS CREATE_COMPLETE DELETE_IN_PROGRESS DELETED";
+var context = {
+	stacks: [],
+	templates: []
+};
+var page = 'stacks';
+
+var API_BASE_URL = 'https://3mq1qnh4p2.execute-api.ap-southeast-2.amazonaws.com/dev/';
+var debug = false;
+
+$(document).ready(function() {
+	$('.navbar-toggle').on('click', function() {
+		$('body').toggleClass('menu');
+	});
+
+	// mark links to current page as active
+	$("#menu a").each(function() {
+		console.log(this.href);
+		if (this.href == window.location.href) {
+			$(this).addClass("active");
+		}
+	});
+
+	// make external links open in new tab/page
+	$(document.links).filter(function() {
+		return this.hostname != window.location.hostname;
+	}).attr('target', '_blank');
+
+
+	$('#fancy-hover li').on('mouseleave', function(a,b,c) {
+		$('#fancy-hover-active').css({
+			width: 0
+		});
+	});
+	$('#fancy-hover li').on('mouseenter', function(event) {
+		event.stopPropagation();
+
+		var $this = $(this);
+
+		var pos = $this.position();
+
+		$('#fancy-hover-active').css({
+			top: pos.top,
+			height: $this.height(),
+			width: '6px'
+		});
+
+	});
+
+	$("#main").on("click", ".template-start", function(event) {
+		console.log('template-start');
+		var $env = $(this).closest(".environment");
+		var id = $env.attr("id").split("-");
+		id.shift();
+		var templateName = id.join('-');
+
+		$('#template-modal .modal-title span').text(templateName);
+		$('#template-modal').modal();
+	});
+
+	$("#template-modal .btn-primary").on("click", function(event) {
+		let templateName = $("#template-modal .modal-title span").text();
+		let stackName = $("#stackname").val();
+		// $env = $('#env-' + stackName);
+		$('#template-modal').modal('hide');
+
+
+		console.log([templateName, stackName]);
+		$.getJSON(API_BASE_URL + 'api/templates/' + templateName + '/quicklaunch/' + stackName, function(data){
+			console.log(data);
+			page = "stacks";
+			loadStacks();
+			// updateEnvironmentStatus($env, data.status);
+		});
+	});
+
+
+	$(".env-start").on("click", function(event) {
+		var $env = $(this).closest(".environment");
+		var [_, pk] = $env.attr("id").split("-");
+		console.log(pk);
+		$.getJSON(API_BASE_URL + 'api/stacks/' + pk + '/launch/', function(data){
+			console.log(data);
+			updateEnvironmentStatus($env, data.status);
+		});
+	});
+	// $(".env-start").on("click", function(event) {
+	// 	var $env = $(this).closest(".environment");
+	// 	var [_, pk] = $env.attr("id").split("-");
+	// 	console.log(pk);
+	// 	$.getJSON('http://localhost:8000/wildcardenv/api/environments/' + pk + '/launch/', function(data){
+	// 		console.log(data);
+	// 		updateEnvironmentStatus($env, data.status);
+	// 	});
+	// });
+
+	$("#main").on("click", ".env-stop", function(event) {
+		console.log('stop');
+		var $env = $(this).closest(".environment");
+		var id = $env.attr("id").split("-");
+		id.shift();
+		var stackName = id.join('-');
+
+		console.log(stackName);
+		//https://1h6df94d9e.execute-api.ap-southeast-2.amazonaws.com/dev
+		$.getJSON(API_BASE_URL + 'api/stacks/' + stackName + '/stop', function(data){
+			console.log(data);
+			updateEnvironmentStatus($env, "DELETE_IN_PROGRESS");
+		});
+	});
+	// $(".env-stop").on("click", function(event) {
+	// 	var $env = $(this).closest(".environment");
+	// 	var [_, pk] = $env.attr("id").split("-");
+	// 	console.log(pk);
+	// 	$.getJSON('http://localhost:8000/wildcardenv/api/environments/' + pk + '/stop/', function(data){
+	// 		console.log(data);
+	// 		updateEnvironmentStatus($env, "DELETE_IN_PROGRESS");
+	// 	});
+	// 	// http://localhost:8000/wildcardenv/api/environments/1/refresh/
+	// });
+
+	// refreshEnvironments();
+	loadStacks();
+	loadTemplates();
+	// var tid = setInterval(refreshEnvironments, 15000);
+
+	$('#nav-templates').click(function(ev) {
+		ev.preventDefault();
+		page = 'templates';
+		refresh();
+	})
+	$('#nav-environments').click(function(ev) {
+		ev.preventDefault();
+		page = 'stacks';
+		refresh();
+	})
+
+});
+
+function loadTemplates() {
+	$.getJSON(API_BASE_URL + 'api/templates', function(data){
+		var templates = data.templates
+		for (var i=0; i < templates.length; i++) {
+			let template = templates[i];
+			console.log(template)
+			context.templates.push(template);
+		}
+		refresh();
+	});
+}
+function loadStacks() {
+	context.stacks = [];
+	$.getJSON(API_BASE_URL + 'api/stacks', function(data){
+		console.log(data)
+		for (let i=0; i<data.length; i++) {
+			let stack = data[i];
+			if (stack.StackName.indexOf('Stack-') > 0) {
+				continue;
+			}
+			stack.StackStatusDisplay = env_status_map[stack.StackStatus];
+			context.stacks.push(stack);
+		}
+		refresh();
+	});
+}
+function refresh() {
+	if (page == 'stacks') {
+		showStacks();
+	}
+	else if (page == 'templates') {
+		showTemplates();
+	}
+
+}
+function showTemplates() {
+	var source   = document.getElementById("templates-template").innerHTML;
+	var template = Handlebars.compile(source);
+	var html    = template(context);
+	$('#main > div > div').first().html(html);
+}
+function showStacks() {
+	var source   = document.getElementById("environments-template").innerHTML;
+	var template = Handlebars.compile(source);
+	var html    = template(context);
+	$('#main > div').html(html);
+}
+function refreshEnvironments() {
+	$.getJSON(API_BASE_URL + 'api/stacks', function(data){
+		console.log(data)
+		for (let i=0; i<data.length; i++) {
+			let stack = data[i];
+			let $env = $('#env-' + stack.StackName);
+			updateEnvironmentStatus($env, stack.StackStatus);
+		}
+	})
+}
+
+function refreshEnvironmentStatus(env) {
+	var $env = $(env);
+	var id = $env.attr("id").split("-");
+	id.shift();
+	var stackName = id.join('-');
+	//var pk = id.split("-")[1];
+
+	$.getJSON(API_BASE_URL + 'api/stacks/' + stackName, function(data){
+		updateEnvironmentStatus($env, data.StackStatus);
+	});
+}
+
+function updateEnvironmentStatus($env, status) {
+	$env.removeClass(all_status_classes);
+	$env.addClass(status);
+	$env.find('.actions .dropdown-toggle').text(env_status_map[status]);
+}
